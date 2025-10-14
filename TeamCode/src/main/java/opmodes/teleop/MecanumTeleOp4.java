2ece89858c857203b196d9fd8f267d3c84450079
athakare <athakarestudy@gmail.com>
2025-10-13 22:21:57 -0400

    intake code

---
+++ TeamCode/src/main/java/opmodes/teleop/MecanumTeleOp4.java
@@ -0,0 +1,53 @@
+package opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class MecanumTeleOp4 extends OpMode {
+    DcMotor frontRightMotor, backRightMotor, frontLeftMotor, backLeftMotor, intakemotor;
+
+    @Override
+    public void init() {
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        intakemotor = hardwareMap.get(DcMotor.class, "intakemotor");
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        double y = gamepad1.left_stick_y;
+        double x = -gamepad1.left_stick_x * 1.1;
+        double rx = -gamepad1.right_stick_x;
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
+
+        // Intake motor control - runs when button A is held down
+        if (gamepad1.a) {
+            intakemotor.setPower(1.0);
+        } else {
+            intakemotor.setPower(0.0);
+        }
+
+        telemetry.addData("Y",-gamepad1.left_stick_y);
+        telemetry.addData("X",-gamepad1.left_stick_x * 1.1);
+        telemetry.addData("RX",gamepad1.right_stick_x);
+        telemetry.addData("Intake Active", gamepad1.a);
+    }
+}
